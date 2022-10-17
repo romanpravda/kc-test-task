0055b99f65d1ac655cb6f2006b7a62a982a95737
Roman Pravda <rp@imagespark.ru>
2022-10-17 15:30:53 +0400

    adding config files

---
+++ config/app.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'key' => 'cXdlcnR5dWlvcGFzZGZnaGprbHp4Y3Zibm0xMjM0NTY3ODkw',
+];
---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'host' => 'mysql.local',
+    'port' => 3306,
+    'database' => 'kc-test-task',
+    'username' => 'default',
+    'password' => 'secret',
+];
